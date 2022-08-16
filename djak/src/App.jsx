3a22f2f173516975ac2b7f08279c7c118c3d69c4
unknown <aminsalkovic2019@gmail.com>
2022-08-16 16:06:51 +0200

    //

--- djak/src/App.jsx
+++ djak/src/App.jsx
@@ -1,14 +1,23 @@
-import React,{useState} from 'react'
+import React from 'react';
 import './App.css';
+import Navbar from './sidebar/Navbar';
+import { Routes,Route, Link} from 'react-router-dom';
+import Home from './sidebar/Home';
+import Reports from './sidebar/Reports';
+import Products from './sidebar/Products';
+import Kard from './components/Kard';
 
-const App=()=> {  
-   
-
-  
+function App() {
   return (
-    <div className="app">
-         
-    </div>
+    <>
+      <Navbar/>
+      <Routes>
+        <Route to='/kard' element={<Kard/>}/>
+        <Route to='/reports' element={<Reports/>}/>
+        <Route to='/products' element={<Products/>}/>
+      </Routes>
+      <h1><Link to='/kard'>amin</Link></h1>
+    </>
   );
 }
 
---
+++ djak/src/slider/SliderData.jsx
@@ -0,0 +1,17 @@
+export const SliderData=[
+    {
+        image:"https://images.unsplash.com/photo-1660536772326-45db48d97082?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw3fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=500&q=60"
+    },
+    {
+        image:'https://images.unsplash.com/photo-1660551772352-0855c10356b1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyN3x8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60'
+    },
+    {
+        image:'https://images.unsplash.com/photo-1660488856923-474e185dba14?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw2N3x8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60'
+    },
+    {
+        image:'https://images.unsplash.com/photo-1660506826403-eb494645a7e2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw2OXx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60'
+    },
+    {
+        image:'https://images.unsplash.com/photo-1660489055865-e075801b0557?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw3OHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60'
+    }
+]
