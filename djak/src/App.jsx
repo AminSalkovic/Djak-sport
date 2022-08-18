32ab1b85acfa127b9d5ca15c05d99005338e3b15
unknown <aminsalkovic2019@gmail.com>
2022-08-18 18:26:19 +0200

    //

--- djak/src/App.jsx
+++ djak/src/App.jsx
@@ -1,6 +1,7 @@
 import React,{useState} from 'react';
 import './App.css';
 import Navbar from './components/Navbar'
+import Navbar2 from './components/Navbar2';
 import { Routes,Route, Link} from 'react-router-dom';
 import Header from './components/Header';
 
@@ -11,6 +12,7 @@ function App() {
     <>
        <Navbar/>
         <Header/>
+       <Navbar2/> 
       
     </>
   );
---
+++ djak/src/components/Navbar2.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import './Navbar2.css'
+
+ const Navbar2 = () => {
+  return (
+    <div className='navbar2'>Navbar2</div>
+  )
+}
+ export default Navbar2;
